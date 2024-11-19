b427ea37562ee665d0bd70487cc45f34bf200ef3
KimSu-70 <rlatngh4770@naver.com>
2024-11-19 11:16:17 +0900

    fix : 1번 문제 풀이
    1. CubeController 스크립트 내용 수정
    2. CubeManager 스크립트 내용 수정

--- Q_01/Assets/Scripts/CubeController.cs
+++ Q_01/Assets/Scripts/CubeController.cs
@@ -6,8 +6,11 @@ public class CubeController : MonoBehaviour
 {
     public Vector3 SetPoint { get; private set; }
 
-    public void SetPosition()
+
+    // 외부에서 값을 받아서 수정 할 수 있게 변경
+    public void SetPosition(Vector3 setpoint)
     {
+        SetPoint = setpoint;
         transform.position = SetPoint;
     }
 }
